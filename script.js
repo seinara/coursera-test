edddf67456d2496f52e04b0e15b08e92487ae056
seinara <94479342+seinara@users.noreply.github.com>
2023-10-24 19:42:24 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// This function will toggle the navigation menu when the user clicks on the hamburger icon.
-function toggleMenu() {
-  var menu = document.querySelector("nav");
-  menu.classList.toggle("open");
-}
-
-// Add an event listener to the hamburger icon to toggle the menu when it is clicked.
-var hamburgerIcon = document.querySelector(".hamburger-icon");
-hamburgerIcon.addEventListener("click", toggleMenu);
